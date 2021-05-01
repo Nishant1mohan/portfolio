cb1a77a0870d6f1b6e880be83256b029288d6e51
Nishant1mohan <57453165+Nishant1mohan@users.noreply.github.com>
2021-05-01 12:04:09 +0530

    Update DesignImg.js

--- src/containers/skills/DesignImg.js
+++ src/containers/skills/DesignImg.js
@@ -4,11 +4,8 @@ export default class DesignImg extends Component {
   render() {
     const theme = this.props.theme;
     return (
-<<<<<<< HEAD
+
       []
-=======
-       {}
->>>>>>> b4e70e0fa1e9bfa524be82d2fad41d59abbba6a8
     );
   }
 }
